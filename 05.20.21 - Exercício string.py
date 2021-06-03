1dab7f824559e5d3db3f28b3e408d3899a5209b4
AdamastorLinsFrancaNetto <adamastorfranca@hotmail.com>
2021-06-03 20:15:11 -0300

    03/06/21 - 20:15

---
+++ 05.20.21 - Exercício string.py
@@ -0,0 +1,21 @@
+#primeiro = "Adamastor "
+#segundo = "Lins "
+#terceiro = "Franca "
+#ultimo = "Netto"
+#nomeCompleto = primeiro + segundo + terceiro + ultimo
+#primUlti = primeiro + ultimo
+#print(nomeCompleto)
+#print("Primeiro = ",primeiro)
+#print("Último = ",ultimo)
+
+#str1 = "Adamastor Lins Franca Netto"
+#print(str1.split())
+#print(str1.upper())
+#print(str1.lower())
+#print(len(str1))
+#print("Silva" in str1)
+#print(str1[0:2])
+#print(str1[:2])
+#print(str1[2:6])
+#print(str1[2:])
+#print(str1[:-2])
